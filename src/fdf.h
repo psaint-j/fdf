ac5f4330b95ee379105372f7b539f1e607e0b123
Philippe SAINT-JACQUES <psaint-j@e1r9p11.42.fr>
2016-02-22 18:07:40 +0100

    X

--- src/fdf.h
+++ src/fdf.h
@@ -6,7 +6,7 @@
 /*   By: psaint-j <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/29 12:17:17 by psaint-j          #+#    #+#             */
-/*   Updated: 2016/02/11 15:36:35 by psaint-j         ###   ########.fr       */
+/*   Updated: 2016/02/22 18:01:33 by psaint-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,8 @@
 #define BUF_SIZE 2048
 #define WIDTH 900
 #define HEIGHT 900
-#define MARGIN 30
+#define MARGIN 20
+#define YOUSSOF 70
 
 typedef struct		s_env
 {
@@ -53,6 +54,7 @@ typedef struct		s_env
 t_env		init_map(t_env env);
 t_env		init_env(t_env env);
 t_env		check_params(t_env env, char *file);
+void		ft_line2(int x1, int y1, int x2, int y2, t_env all);
 void		ft_tabstr(char **z, int y, char ***map);
 void		ft_print_tab(char **tab);
 void		print_mlx(t_env env, char **map, int x);
--- src/ft_draw.c
+++ src/ft_draw.c
@@ -6,7 +6,7 @@
 /*   By: psaint-j <psaint-j@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 08:03:06 by psaint-j          #+#    #+#             */
-/*   Updated: 2016/02/15 19:11:39 by psaint-j         ###   ########.fr       */
+/*   Updated: 2016/02/22 18:05:20 by psaint-j         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,187 @@
 #define U s.key_up
 #define D s.key_down
 
-void	ft_line(int x1, int x2, int y1, int y2, void *win, 		void *mlx)
-{
-	int 	dy;
-	int		dx;
-	int		e;
 
-	dy = (y2 - y1) * 2;
-	e = x1				 - x2;
-	dx = e * 2;
-	while (x1 <= x2)
+void	ft_line2(int x1, int y1, int x2, int y2, t_env all)
+{
+	int dx;
+	int dy;
+	int	e;
+
+	dx = x2 - x1;
+	dy = y2 - y1;
+	if (	dx != 0)
 	{
-		mlx_pixel_put(mlx, win, x1, y1, 0x00FFFFFF);
-		x1++;
-		if ((e = e - dy) <= 0)
+		if (dx > 0)
 		{
-			y1++;
-			e = e + dx;
+			if (dy != 0)
+			{
+				if (dy > 0)
+				{
+					if (dx >= dy)
+					{
+						e = dx;
+						dx = e * 2;
+						dy = dy * 2;
+						while (x1 != x2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e - dy) < 0)
+							{
+								y1 = y1 + 1;
+								e = e + dx;
+							}
+							++x1;
+						}
+					}
+					else
+					{
+						e = dy;
+						dy = e * 2;
+						dx = dx * 2;
+						while (++y1 != y2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e - dx) < 0)
+							{
+								x1 = x1 + 1;
+								e = e + dy;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (dx >= -dy)
+					{
+						e = dx;
+						dx = e * 2;
+						dy = dy * 2;
+						while ((x1 = x1 + 1) != x2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e + dy) < 0)
+							{
+								y1 = y1 - 1;
+								e = e + dx;
+							}
+						}
+					}
+					else
+					{
+						e = dy;
+						dy = e * 2;
+						dx = dx * 2;
+						while ((y1 = y1 - 1) != y2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e + dx) < 0)
+							{
+								x1 = x1 + 1;
+								e = e + dy;
+							}
+						}
+					}
+				}
+			}
+			else
+			{
+				while ((x1 = x1 + 1) != x2)
+					mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+			}
+		}
+		else
+		{
+			if ((dy = y2 - y1) != 0)
+			{
+				if (dy > 0)
+				{
+					if (-dx >= dy)
+					{
+						e = dx;
+						dx = e * 2;
+						dy = dy * 2;
+						while ((x1 = x1 - 1) != x2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e + dy) != 0)
+							{
+								y1 = y1 + 1;
+								e = e + dx;
+							}
+						}
+					}
+					else
+					{
+						e = dy;
+						dy = e * 2;
+						dx = dx * 2;
+						while ((y1 = y1 + 1) != y2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e + dx) != 0)
+							{
+								x1 = x1 - 1;
+								e = e + dy;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (dx <= dy)
+					{
+						e = dx;
+						dx = e * 2;
+						dy = dy * 2;
+						while ((x1  = x1 - 1) != x2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e - dy) >= 0)
+							{
+								y1 = y1 - 1;
+								e = e + dx;
+							}
+						}
+					}
+					else
+					{
+						e = dy;
+						dy = e * 2;
+						dx = dx * 2;
+						while ((y1  = y1 - 1) != y2)
+						{
+							mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+							if ((e = e - dx) >= 0)
+							{
+								x1 = x1 - 1;
+								e = e + dy;
+							}
+						}
+					}
+				}
+			}
+			else // dy = 0 and dx < 0
+			{
+				while ((x1 = x1 - 1) != x2)
+					mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);					
+			}
+		}
+	}
+	else // dx = 0
+	{
+		if ((dy = y2 - y1) != 0)
+		{
+			if (dy > 0)
+			{
+				while ((y1 = y1 + 1) != y2)
+					mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+			}
+			else // dy < 0 and dx = 0
+			{
+				while ((y1 = y1 - 1) != y2)
+					mlx_pixel_put(all.mlx, all.win, x1, y1, 0xFFFFFFF);
+			}
 		}
 	}
 }
@@ -49,16 +213,18 @@ void	ft_draw_pixel(t_env s)
 			//{
 			//while (s.j < MARGIN)
 			//{
-				s.isoY = (s.e_y - s.e_x);
-				s.isoX = ((s.e_x + s.e_y)/2);
-				mlx_pixel_put(s.mlx, s.win, s.j + s.isoY + WIDTH/3, s.isoX, 0xFFFFF);
-				mlx_pixel_put(s.mlx, s.win, s.isoY + WIDTH/3, s.isoX + s.j, 0xFFFFF);
-				//other side
-				mlx_pixel_put(s.mlx, s.win, s.j + s.isoY + WIDTH/3, s.isoX + MARGIN, 0xFFFFF);
-				mlx_pixel_put(s.mlx, s.win, s.isoY + WIDTH/3 + MARGIN, s.isoX + s.j, 0xFFFFF);
-				s.j++;
-		//	}
-			s.j = 0;
+			s.isoY = (s.e_y - s.e_x);
+			s.isoX = ((s.e_x + s.e_y)/2);
+			ft_line2(s.isoY + 250, s.isoX + 50, s.isoY + 270, s.isoX + 60, s);
+			ft_line2(s.isoY + 270, s.isoX + 40, s.isoY + 250, s.isoX + 50, s);
+			//mlx_pixel_put(s.mlx, s.win, s.isoY + WIDTH/3, s.isoX, 0xFFFFF);
+			//mlx_pixel_put(s.mlx, s.win, s.isoY + WIDTH/3, s.isoX + s.j, 0xFFFFF);
+			//other side
+			//mlx_pixel_put(s.mlx, s.win, s.j + s.isoY + WIDTH/3, s.isoX + MARGIN, 0xFFFFF);
+			//mlx_pixel_put(s.mlx, s.win, s.isoY + WIDTH/3 + MARGIN, s.isoX + s.j, 0xFFFFF);
+			//s.j++;
+			//	}
+			//s.j = 0;
 			/*	}
 				else
 				{
